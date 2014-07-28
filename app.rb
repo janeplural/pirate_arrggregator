02c855c22bf7791cad7ee6de5ab492e92993db59
janeplural <mlle.jihshi@gmail.com>
2014-07-28 17:37:19 -0400

    updated db info for heroku

--- app.rb
+++ app.rb
@@ -1,12 +1,21 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 
-db = URI.parse('postgres://Jane:pass@localhost/players')
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://Jane:pass@localhost/players')
 
-ActiveRecord::Base.establish_connection({
-  database: db.path[1..-1],
-  adapter: "postgresql"
-})
+# ActiveRecord::Base.establish_connection({
+#   database: db.path[1..-1],
+#   adapter: "postgresql"
+# })
+
+ActiveRecord::Base.establish_connection(
+ :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+ :host     => db.host,
+ :username => db.user,
+ :password => db.password,
+ :database => db.path[1..-1],
+ :encoding => 'utf8'
+)
 
 class Player < ActiveRecord::Base
 end
